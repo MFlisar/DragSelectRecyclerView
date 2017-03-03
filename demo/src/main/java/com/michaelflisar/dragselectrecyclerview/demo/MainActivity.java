8775e38955986e39664970c0da2494f44a85e1cf
MichaelFlisar <mflisar85@gmail.com>
2017-03-03 08:58:47 +0100

    simple demo added

---
+++ demo/src/main/java/com/michaelflisar/dragselectrecyclerview/demo/MainActivity.java
@@ -0,0 +1,82 @@
+package com.michaelflisar.dragselectrecyclerview.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.michaelflisar.dragselectrecyclerview.DragSelectTouchListener;
+
+public class MainActivity extends AppCompatActivity
+{
+    private DragSelectTouchListener mDragSelectTouchListener;
+    private  TestAutoDataAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Demo");
+        setSupportActionBar(toolbar);
+
+        // 1) Prepare the RecyclerView (init LayoutManager and set Adapter)
+        RecyclerView rvData = (RecyclerView) findViewById(R.id.rvData);
+        GridLayoutManager glm = new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false);
+        rvData.setLayoutManager(glm);
+        mAdapter = new TestAutoDataAdapter(this, 500);
+        rvData.setAdapter(mAdapter);
+        mAdapter.setClickListener(new TestAutoDataAdapter.ItemClickListener()
+        {
+            @Override
+            public void onItemClick(View view, int position)
+            {
+                mAdapter.updateSelection(position, true);
+            }
+
+            @Override
+            public boolean onItemLongClick(View view, int position)
+            {
+                // if one item is long pressed, we start the drag selection like following:
+                // we just call this function and pass in the position of the first selected item
+                mAdapter.updateSelection(position, true);
+                mDragSelectTouchListener.startDragSelection(position);
+                return true;
+            }
+        });
+
+        // 2) Add the DragSelectListener
+        mDragSelectTouchListener = new DragSelectTouchListener()
+                .withSelectListener(new DragSelectTouchListener.OnDragSelectListener()
+                {
+                    @Override
+                    public void onSelectChange(int start, int end, boolean isSelected)
+                    {
+                        // update your selection
+                        // range is inclusive start/end positions
+                        for (int i = start; i <= end; i++)
+                            mAdapter.updateSelection(i, isSelected);
+                    }
+                });
+        rvData.addOnItemTouchListener(mDragSelectTouchListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_clear)
+            mAdapter.deselectAll();
+        return true;
+    }
+}
---
+++ demo/src/main/java/com/michaelflisar/dragselectrecyclerview/demo/TestAutoDataAdapter.java
@@ -0,0 +1,128 @@
+package com.michaelflisar.dragselectrecyclerview.demo;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.HashSet;
+
+/**
+ * Created by flisar on 03.03.2017.
+ */
+
+public class TestAutoDataAdapter extends RecyclerView.Adapter<TestAutoDataAdapter.ViewHolder>
+{
+
+    private int mDataSize;
+    private Context mContext;
+    private ItemClickListener mClickListener;
+
+    private HashSet<Integer> mSelected;
+
+    public TestAutoDataAdapter(Context context, int size)
+    {
+        mContext = context;
+        mDataSize = size;
+        mSelected = new HashSet<>();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.test_cell, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position)
+    {
+        holder.tvText.setText(String.valueOf(position));
+        if (mSelected.contains(position))
+            holder.tvText.setBackgroundColor(Color.RED);
+        else
+            holder.tvText.setBackgroundColor(Color.WHITE);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return mDataSize;
+    }
+
+    // ----------------------
+    // Selection
+    // ----------------------
+
+    public void updateSelection(int pos, boolean selected)
+    {
+        if (selected)
+            mSelected.add(pos);
+        else
+        mSelected.remove(pos);
+        notifyItemChanged(pos);
+    }
+
+    public void deselectAll()
+    {
+        // this is not beautiful...
+        mSelected.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getCountSelected()
+    {
+        return mSelected.size();
+    }
+
+    // ----------------------
+    // Click Listener
+    // ----------------------
+
+    public void setClickListener(ItemClickListener itemClickListener)
+    {
+        mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener
+    {
+        void onItemClick(View view, int position);
+        boolean onItemLongClick(View view, int position);
+    }
+
+    // ----------------------
+    // ViewHolder
+    // ----------------------
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener
+    {
+        public TextView tvText;
+
+        public ViewHolder(View itemView)
+        {
+            super(itemView);
+            tvText = (TextView) itemView.findViewById(R.id.tvText);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view)
+        {
+            if (mClickListener != null)
+                mClickListener.onItemClick(view, getAdapterPosition());
+        }
+
+        @Override
+        public boolean onLongClick(View view)
+        {
+            if (mClickListener != null)
+                return mClickListener.onItemLongClick(view, getAdapterPosition());
+            return false;
+        }
+    }
+}
